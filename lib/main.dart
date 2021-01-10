21246b125a027035f65b4092171a21e3b632b46e
MuhammadUsmanIbraheem <usmanibraheem626@gmail.com>
2021-01-10 19:20:46 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'ui/climatefile.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Climate',
+      home: Climate(),
+    ),
+  );
+}
---
+++ lib/ui/climatefile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Climate extends StatefulWidget {
+  @override
+  _ClimateState createState() => _ClimateState();
+}
+
+class _ClimateState extends State<Climate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ClimateApp'),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
